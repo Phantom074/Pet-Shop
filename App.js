fa1e876d4fb02dcfdc0f161cc0ae475d412ea3c1
Mukul <106223126+Phantom074@users.noreply.github.com>
2024-07-05 19:20:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+// import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Left-menu></Left-menu>
+        
+    )
+  }
+}
+
+export default App;
